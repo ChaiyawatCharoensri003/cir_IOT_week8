d47e12c725784bdf45245a891026947163866da4
Aruza354 <Aruza354@gmail.com>
2022-12-08 17:42:15 +0700

    update

---
+++ week804.js
@@ -0,0 +1,11 @@
+//Anonymous function
+function sombut(){
+    return `My name is sombut`
+}
+console.log(sombut())
+
+//Anonymous function
+const ratthapoom = function (){
+    return `My name is Ratthapoom`
+}
+console.log(ratthapoom())
